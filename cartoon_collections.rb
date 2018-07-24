a284619412cc8b5b3004b9fa0d4dae8791ea5fea
Adam Johnson <adamwjo@gmail.com>
2018-07-24 15:22:20 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,8 +12,14 @@ def summon_captain_planet(elements)
   end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.any? do |word|
+    if word.length <= 4
+      true
+    else
+      false
+    end
+  end
 end
 
 def find_the_cheese# code an argument here
